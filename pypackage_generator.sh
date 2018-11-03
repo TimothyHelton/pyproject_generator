3884bd4fed17d13c4e335a70c3d624d3c827e3e1
Timothy Helton <timothy.j.helton@gmail.com>
2018-11-03 07:17:40 -0600

    ENH: license refactored

--- pypackage_generator.sh
+++ pypackage_generator.sh
@@ -326,35 +326,35 @@ git_init() {
 
 
 license() {
-    txt="Copyright (c) ${YEAR}, ${AUTHOR}.\n"
-    txt+="All rights reserved.\n"
-    txt+="\n"
-    txt+="Redistribution and use in source and binary forms, with or without\n"
-    txt+="modification, are permitted provided that the following conditions are met:\n"
-    txt+="\n"
-    txt+="* Redistributions of source code must retain the above copyright notice, this\n"
-    txt+="  list of conditions and the following disclaimer.\n"
-    txt+="\n"
-    txt+="* Redistributions in binary form must reproduce the above copyright notice,\n"
-    txt+="  this list of conditions and the following disclaimer in the documentation\n"
-    txt+="  and/or other materials provided with the distribution.\n"
-    txt+="\n"
-    txt+="* Neither the name of the ${MAIN_DIR} Developers nor the names of any\n"
-    txt+="  contributors may be used to endorse or promote products derived from this\n"
-    txt+="  software without specific prior written permission.\n"
-    txt+="\n"
-    txt+="THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\n"
-    txt+="AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n"
-    txt+="IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\n"
-    txt+="DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR\n"
-    txt+="ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES\n"
-    txt+="(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;\n"
-    txt+="LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON\n"
-    txt+="ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT\n"
-    txt+="(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS\n"
-    txt+="SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n"
-
-    printf %b "${txt}" >> "${MAIN_DIR}${FILE_SEP}LICENSE.txt"
+    printf "%s\n" \
+        "Copyright (c) ${YEAR}, ${AUTHOR}." \
+        "All rights reserved." \
+        "" \
+        "Redistribution and use in source and binary forms, with or without" \
+        "modification, are permitted provided that the following conditions are met:" \
+        "" \
+        "* Redistributions of source code must retain the above copyright notice, this" \
+        "  list of conditions and the following disclaimer." \
+        "" \
+        "* Redistributions in binary form must reproduce the above copyright notice," \
+        "  this list of conditions and the following disclaimer in the documentation" \
+        "  and/or other materials provided with the distribution." \
+        "" \
+        "* Neither the name of the ${MAIN_DIR} Developers nor the names of any" \
+        "  contributors may be used to endorse or promote products derived from this" \
+        "  software without specific prior written permission." \
+        "" \
+        "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"" \
+        "AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE" \
+        "IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE" \
+        "DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR" \
+        "ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES" \
+        "(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;" \
+        "LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON" \
+        "ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT" \
+        "(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS" \
+        "SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE." \
+        > "${MAIN_DIR}${FILE_SEP}LICENSE.txt"
 }
 
 
